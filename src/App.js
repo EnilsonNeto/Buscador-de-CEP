866d4a3a5b6b849d9ec6c3038a94f72478a162d3
EnilsonNeto <enilsonneto8@gmail.com>
2022-12-21 17:24:24 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+    
+    
+      <div className="containerInput">
+          <input type="text" placeholder="Digite aqui seu CEP">
+
+          </input>
+
+          <button className="buttonSearch">
+            Procurar
+          </button>
+      </div>
+
+
+
     </div>
   );
 }
