ecb2ae2a25d6dda40e7d3b261035b6f94a17b195
EnilsonNeto <enilsonneto8@gmail.com>
2022-12-21 20:11:59 -0300

    Colocando em pratica as funções ultilizando "API" no aplicativo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
+import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi'
+import  api  from './services/api';
 import './styles.css'
 function App() {
+  const [input, setInput] = useState('')
+  
+  async function handleSearch () {
+
+    if(input === '') {
+      alert("Preencha o campo!")
+      return;
+    }
+
+    try{
+      const response = await api.get(`${input}/json`);
+      console.log(response.data);
+    }catch{
+      alert("Erro ao buscar esta informação!");
+      setInput("");
+    }
+
+  }
+
   return (
     <div className="container">
       <h1 className="title">Buscador CEP</h1>
     
     
       <div className="containerInput">
-          <input type="text" placeholder="Digite aqui seu CEP">
+          <input type="text"
+           placeholder="Digite aqui seu CEP"
+           value={input}
+           onChange={(e) => setInput(e.target.value) }
+           />
 
-          </input>
 
-          <button className="buttonSearch">
+          <button className="buttonSearch" onClick={handleSearch}>
             <FiSearch size={25} color="#fff"/>
           </button>
       </div>
